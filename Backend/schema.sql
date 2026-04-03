26e00e47ea6e557f4624ae23223f414bda69653d
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-04-03 21:58:04 +0300

    feat: mineral and vitamin tables

--- Backend/schema.sql
+++ Backend/schema.sql
@@ -27,15 +27,6 @@ CREATE TABLE Meal (
     Servings INT NOT NULL
 );
 
-CREATE TABLE Mineral (
-    Id INT PRIMARY KEY IDENTITY(1, 1),
-    IngredientId INT NOT NULL UNIQUE,
-    Chloride DECIMAL(18, 9),
-    Sodium DECIMAL(18, 9),
-
-    FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id)
-);
-
 CREATE TABLE MealIngredient (
     Id INT PRIMARY KEY IDENTITY(1, 1),
     IngredientId INT NOT NULL,
@@ -44,4 +35,47 @@ CREATE TABLE MealIngredient (
 
     FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id),
     FOREIGN KEY (MealId) REFERENCES Meal(Id)
+);
+
+CREATE TABLE Mineral (
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    IngredientId INT NOT NULL UNIQUE,
+    Calcium DECIMAL(9, 2),
+    Chloride DECIMAL(9, 2),
+    Chromium DECIMAL(9, 2),
+    Copper DECIMAL(9, 2),
+    Fluoride DECIMAL(9, 2),
+    Iodine DECIMAL(9, 2),
+    Iron DECIMAL(9, 2),
+    Magnesium DECIMAL(9, 2),
+    Manganese DECIMAL(9, 2),
+    Molybdenum DECIMAL(9, 2),
+    Phosphorus DECIMAL(9, 2),
+    Potassium DECIMAL(9, 2),
+    Selenium DECIMAL(9, 2),
+    Sodium DECIMAL(9, 2),
+    Zinc DECIMAL(9, 2),
+
+    FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id)
+);
+
+CREATE TABLE Vitamin (
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    IngredientId INT NOT NULL UNIQUE,
+    Biotin DECIMAL(9, 2),
+    Choline DECIMAL(9, 2),
+    Folate DECIMAL(9, 2),
+    Niacin DECIMAL(9, 2),
+    PantothenicAcid DECIMAL(9, 2),
+    Riboflavin DECIMAL(9, 2),
+    Thiamin DECIMAL(9, 2),
+    VitaminA DECIMAL(9, 2),
+    VitaminB12 DECIMAL(9, 2),
+    VitaminB6 DECIMAL(9, 2),
+    VitaminC DECIMAL(9, 2),
+    VitaminD DECIMAL(9, 2),
+    VitaminE DECIMAL(9, 2),
+    VitaminK DECIMAL(9, 2),
+
+    FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id)
 );
