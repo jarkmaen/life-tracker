9e096c46237f127f117b7408dc6ca5d090608767
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-04-18 23:12:54 +0300

    chore: initial frontend

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        babel({ presets: [reactCompilerPreset()] }),
+        react(),
+        tailwindcss()
+    ]
+});
